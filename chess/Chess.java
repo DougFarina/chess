d10566e8fb964c97597d55f7d63d8d1af7154eff
DougFarina <dougfarinaccio1@gmail.com>
2026-02-24 17:02:04 -0500

    Add rook move handling and path checks

--- chess/Chess.java
+++ chess/Chess.java
@@ -69,19 +69,23 @@ public class Chess {
 			return rp;
 		}
 
-		// Placeholder: parsing + basic source/turn/destination validation are implemented,
+		// Placeholder: parsing + basic source/turn/destination validation are
+		// implemented,
 		// but move execution is not yet.
-		// Move Execution: Should be moved to separate methods 
+		// Move Execution: Should be moved to separate methods
 		switch (pieceType(movingPiece)) {
 			case 'P':
 				if (targetPiece == null) {// Pawn movement with no target piece
-					// If there is no target piece, a pawn can only move to the same file position and only a maximum distance of 2
-					if ((movingPiece.pieceFile != parsed.to.file) || Math.abs(parsed.to.rank - movingPiece.pieceRank) > 2) {
+					// If there is no target piece, a pawn can only move to the same file position
+					// and only a maximum distance of 2
+					if ((movingPiece.pieceFile != parsed.to.file)
+							|| Math.abs(parsed.to.rank - movingPiece.pieceRank) > 2) {
 						rp.message = ReturnPlay.Message.ILLEGAL_MOVE;
 						return rp;
 					}
-					//Will move pawn two spaces only if it's on its starting rank
-					if (Math.abs(parsed.to.rank - movingPiece.pieceRank) == 2) { //Checks if pawn wants to move 2 spaces
+					// Will move pawn two spaces only if it's on its starting rank
+					if (Math.abs(parsed.to.rank - movingPiece.pieceRank) == 2) { // Checks if pawn wants to move 2
+																					// spaces
 						if (movingPiece.pieceRank == 2 || movingPiece.pieceRank == 7) {
 							for (ReturnPiece p : rp.piecesOnBoard) {
 								if (movingPiece.equals(p)) {
@@ -95,25 +99,81 @@ public class Chess {
 						rp.message = ReturnPlay.Message.ILLEGAL_MOVE;
 						return rp;
 					}
-					// will move pawn one space up 
-					if (Math.abs(parsed.to.rank - movingPiece.pieceRank) == 1) { //Checks if pawn wants to move 2 spaces
+					// will move pawn one space up
+					if (Math.abs(parsed.to.rank - movingPiece.pieceRank) == 1) { // Checks if pawn wants to move 2
+																					// spaces
 						for (ReturnPiece p : rp.piecesOnBoard) {
 							if (movingPiece.equals(p)) {
 								p.pieceFile = parsed.to.file;
 								p.pieceRank = parsed.to.rank;
 							}
 						}
-					}
-				}/*  else {
-					char targetFile = Integer.("" + targetPiece.pieceFile);
-					String movingFile = "" + movingPiece.pieceFile;
-					if (targetPiece.pieceRank == movingPiece.pieceRank + 1 || ((char)targetPiece.pieceFile - (char)movingPiece.pieceFile) == 1) {
-
-					}
-				}*/
+					} 
+				} /*
+					 * else {
+					 * char targetFile = Integer.("" + targetPiece.pieceFile);
+					 * String movingFile = "" + movingPiece.pieceFile;
+					 * if (targetPiece.pieceRank == movingPiece.pieceRank + 1 ||
+					 * ((char)targetPiece.pieceFile - (char)movingPiece.pieceFile) == 1) {
+					 * 
+					 * }
+					 * }
+					 */
 
 				break;
 			case 'R':
+				boolean sameFile = movingPiece.pieceFile == parsed.to.file; // this is basically just making sure the
+																			// rook is moving horizontal
+				boolean sameRank = movingPiece.pieceRank == parsed.to.rank; // same thign as ^ but for vertical
+
+				if (!sameFile && !sameRank) {
+					rp.message = ReturnPlay.Message.ILLEGAL_MOVE; // if the rook tries to move in a diagonal move it
+																	// will print the illegal move message
+					return rp;
+				}
+
+				if (sameFile) { //vertical move check
+					int step;
+					if (parsed.to.rank > movingPiece.pieceRank) {
+						step = 1;
+					} else {
+						step = -1;
+					}												//this will stop before you get to target or hit something
+					for (int rank = movingPiece.pieceRank + step; rank != parsed.to.rank; rank += step) {
+						if (findPieceAt(movingPiece.pieceFile, rank) != null) {
+							rp.message = ReturnPlay.Message.ILLEGAL_MOVE;
+							return rp;
+						}
+					}
+					} else { //horizontal move check
+						char fromFile = movingPiece.pieceFile.toString().charAt(0);
+						char toFile = parsed.to.file.toString().charAt(0);
+						int step;
+						if (toFile > fromFile) {
+							step = 1;
+						} else {
+							step = -1;
+						}
+						for (char fileChar = (char) (fromFile + step); fileChar != toFile; fileChar += step) {
+							ReturnPiece.PieceFile file = ReturnPiece.PieceFile.valueOf(String.valueOf(fileChar)); //same logic as vertical but I had to make fromFile a char since its an enum unlike rank
+							if (findPieceAt(file, movingPiece.pieceRank) != null) {
+								rp.message = ReturnPlay.Message.ILLEGAL_MOVE;
+								return rp;
+							}
+					}
+				}
+
+				// Capture if destination has an opponent piece. I already made sure that this wont work if the attacked piece is same color
+				if (targetPiece != null) {
+					rp.piecesOnBoard.remove(targetPiece); //remove is an ArrayList method and I think it works fine
+				}
+
+				for (ReturnPiece p : rp.piecesOnBoard) { //this actually moves the piece on the boar
+					if (movingPiece.equals(p)) {
+						p.pieceFile = parsed.to.file;
+						p.pieceRank = parsed.to.rank;
+					}
+				}
 				break;
 			case 'N':
 				break;
@@ -203,8 +263,7 @@ public class Chess {
 			return invalidMove();
 		}
 
-
-		move.from = parseSquare(tokens[idx++]); //if both of these are null then move is invalid
+		move.from = parseSquare(tokens[idx++]); // if both of these are null then move is invalid
 		move.to = parseSquare(tokens[idx++]);
 		if (move.from == null || move.to == null) {
 			return invalidMove();
